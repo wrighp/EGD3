451000f4dcd561844be466e17f6bb10a31dbf00b
wrighp <wrightCoder@gmail.com>
2018-02-28 16:54:02 -0500

    :sparkles: Characters added to line when bumbed

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
-public class Follower : MonoBehaviour {
+public class Follower : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool following = false;
+    AICharacterControl target;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GetComponent<AICharacterControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(!following && collision.gameObject.name == "Player")
+        {
+            Leader leader = collision.gameObject.GetComponent<Leader>();
+            Transform targ;
+            if (leader.line.Count == 0)
+            {
+                targ = leader.transform;
+            }
+            else
+            {
+                targ = leader.line.Last.Value.transform;
+            }
+            target.target = targ;
+            leader.line.AddLast(this);
+            following = true;
+        }
+    }
 }
