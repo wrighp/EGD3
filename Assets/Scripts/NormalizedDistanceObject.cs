4f1ed5b4844f94148b288ed5ca90ad5c6a37f4ae
Unknown <wrightCoder@gmail.com>
2018-04-03 23:00:03 -0400

    ♻️ Many changes and updates/fixes

---
+++ Assets/Scripts/NormalizedDistanceObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class NormalizedDistanceObject : MonoBehaviour {
+
+    public Transform targetTransform;
+    public float scale = 1f;
+
+    Rigidbody rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (targetTransform != null)
+        {
+            targetTransform.position = Vector3.Lerp(targetTransform.position, transform.position + rigidbody.velocity * scale, .5f);
+        }
+	}
+}
