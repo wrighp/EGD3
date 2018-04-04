de9d0e83cc59ca6485b71c4af4ca81b1a74ae848
Unknown <wrightCoder@gmail.com>
2018-04-03 20:36:45 -0400

    :sparkles: Follower Jumping

--- Assets/Scripts/Leader.cs
+++ Assets/Scripts/Leader.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Leader : MonoBehaviour {
 
@@ -12,8 +14,11 @@ public class Leader : MonoBehaviour {
 		
 	}
 
-    bool triggerPressed = false;
+    public float jumpForce = 4f;
+    public ForceMode jumpForceMode = ForceMode.VelocityChange;
+    public float jumpTimeDelay = .25f;
 
+    bool triggerPressed = false;
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Drop"))
@@ -50,10 +55,45 @@ public class Leader : MonoBehaviour {
             triggerPressed = false;
         }
 
+        //Needs grounded checks
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            float duration = 0;
+            foreach(var follower in line)
+            {
+                duration += jumpTimeDelay;
+                StartCoroutine(FollowerJumper(follower.GetComponent<Rigidbody>(), duration));
+            }
+        }
+
+    }
+    //Needs grounded checks
+    IEnumerator FollowerJumper(Rigidbody rigidbody, float delayTime)
+    {
+
+        yield return new WaitForSeconds(delayTime);
+        //Debug.Log("Jump " + rigidbody.name);
+        if (rigidbody == null)
+        {
+            yield break;
+        }
+        NavMeshAgent agent = rigidbody.GetComponent<NavMeshAgent>();
+        agent.updatePosition = false;
+        //agent.isStopped = true;
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpForce, rigidbody.velocity.z);
+        //rigidbody.AddForce(0, jumpForce, 0, jumpForceMode);
+        agent.velocity = rigidbody.velocity;
+
+        //Or until grounded
+        yield return new WaitForSeconds(.75f);
+
+        agent.updatePosition = true;
+       // agent.isStopped = false;
     }
 
     private void performHug() {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position + new Vector3(0, .5f, 1.5f), 3f);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position + transform.rotation * new Vector3(0, .5f, 1.5f), 1.5f);
         int i = 0;
         while (i < hitColliders.Length) {
             Follower f = hitColliders[i].gameObject.GetComponent<Follower>();
