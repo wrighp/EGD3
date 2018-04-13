03c9982e83e828a5db25d79cb50265b1bc90512e
JStiggy <johnstiggy@gmail.com>
2018-04-13 19:47:58 -0400

    :camera: Added fade in and fade out systems

---
+++ Assets/Scripts/FadeSystem.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class FadeSystem : MonoBehaviour
+{
+
+    public static FadeSystem i;
+
+    public Texture2D fadeTexture;
+    float fadeSpeed = 0.2f;
+    int drawDepth = -1000;
+
+    float alpha = 0;
+    int fadeDir = 0;
+    bool fading = false;
+
+    void Start() {
+        i = this;
+        PerformFadeIn();
+    }
+
+    public IEnumerator PerformFade(object[] values)
+    {
+        fading = true;
+        fadeDir = (int)values[0];
+        alpha = Mathf.Clamp01(1 - fadeDir);
+        while (!(alpha >= 1 && fadeDir == 1) && !(alpha <= 0 && fadeDir == -1)) {
+            
+            yield return null;
+        }
+
+        if (alpha >= 1 && fadeDir == 1 && values[1] != null) {
+            SceneManager.LoadScene((string)values[1]);
+            this.StopAllCoroutines();
+        }
+        else {
+            fading = false;
+        }
+        yield return null;
+    }
+
+    public IEnumerator PerformMultiFade()
+    {
+        fading = true;
+        fadeDir = 1;
+        alpha = 0;
+        while (!(alpha >= 1 && fadeDir == 1) && !(alpha <= 0 && fadeDir == -1))
+        {
+
+            yield return null;
+        }
+        fadeDir = -1;
+        alpha = 1;
+        while (!(alpha >= 1 && fadeDir == 1) && !(alpha <= 0 && fadeDir == -1))
+        {
+
+            yield return null;
+        }
+
+        fading = false;
+
+        yield return null;
+    }
+
+    public IEnumerator PerformSpriteFade(object[] vals)
+    {
+        SpriteRenderer sr = ((GameObject)vals[0]).GetComponent<SpriteRenderer>();
+        float fade = (bool)vals[1] ? 0 : 1;
+        float fSpeed = fadeSpeed;
+        while (true)
+        {
+            if ((bool)vals[1])
+            {
+                //FadeIn
+                fade = Mathf.SmoothDamp(fade, 1f, ref fSpeed, .2f);
+                sr.color = new Color(1f, 1f, 1f, fade);
+                if (fade >= .95)
+                {
+                    sr.color = new Color(1f, 1f, 1f, 1f);
+                    break;
+                }
+            }
+            else
+            {
+                fade = Mathf.SmoothDamp(fade, 0f, ref fSpeed, .2f);
+                sr.color = new Color(1f, 1f, 1f, fade);
+                if (fade <= 0.05)
+                {
+                    sr.color = new Color(1f, 1f, 1f, 0f);
+                    break;
+                }
+
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+
+    void OnGUI()
+    {
+        if (fading == false)
+            return;
+
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+
+        GUI.depth = drawDepth;
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+    }
+
+    public void PerformFadeIn() {
+        object[] tmp = { -1, null };
+        GetComponent<FadeSystem>().StartCoroutine("PerformFade", tmp);
+    }
+
+    public void PerformFadeOut(string scene = null)
+    {
+        object[] tmp = { 1, scene };
+        GetComponent<FadeSystem>().StartCoroutine("PerformFade", tmp);
+    }
+
+    public void PerformMultiFadeCall(string scene = null)
+    {
+        object[] tmp = { 1, null };
+        GetComponent<FadeSystem>().StartCoroutine("PerformMultiFade", tmp);
+    }
+}
