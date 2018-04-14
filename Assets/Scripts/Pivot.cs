a9cfb7bb3855a6c3b11c447e6d173b1bd7aba756
JStiggy <johnstiggy@gmail.com>
2018-04-13 21:34:45 -0400

    :camera: Setup for rendering followers on UI

---
+++ Assets/Scripts/Pivot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pivot : MonoBehaviour {
+
+    public float speed = 10f;
+    public float maxRotation = 30f;
+    void Update() {
+        // Slowly rotate the object arond its X axis at 1 degree/second.
+        transform.rotation = Quaternion.Euler(0f, maxRotation * (Mathf.Sin(Time.time * speed)), 0f);
+    }
+}
