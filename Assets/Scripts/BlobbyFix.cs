5dba9c1600ca3216f72179f3e90dea854b84a791
John Steigerwald <johnstiggy@gmail.com>
2018-04-14 12:40:22 -0400

    :bug: Music and Blobby fix

---
+++ Assets/Scripts/BlobbyFix.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobbyFix : MonoBehaviour {
+    public Material mat;
+
+    // Use this for initialization
+    void Start()
+    {
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();  // get the first Skinned mesh renderer component (only active ones!) in the objects children
+        skinnedMeshRenderer.material = mat;
+    }
+}
