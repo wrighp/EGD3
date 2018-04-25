35827674d8d99b9da34a8b3d9a15242a06605068
JStiggy <johnstiggy@gmail.com>
2018-04-24 22:45:57 -0400

    :bug: Box now has a mesh to render

---
+++ Assets/PNGCreate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNGCreate : MonoBehaviour {
+
+    RenderTexture[] rTs;
+
+	// Use this for initialization
+	void Start () {
+        rTs = Resources.LoadAll<RenderTexture>("RenderTexture");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.L)) {
+            
+            foreach (RenderTexture rt in rTs) {
+                print(rt.name);
+                RenderTexture currentActiveRT = RenderTexture.active;
+                RenderTexture.active = rt;
+                Texture2D tex = new Texture2D(rt.width, rt.height);
+                tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+                var bytes = tex.EncodeToPNG();
+                System.IO.File.WriteAllBytes("\\Users\\Stiggy\\Desktop\\"+rt.name+".png", bytes);
+                UnityEngine.Object.Destroy(tex);
+                RenderTexture.active = currentActiveRT;
+            }
+        }
+
+	}
+}
